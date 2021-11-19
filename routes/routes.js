4067278fd2cf6c1f596209b6afb166ccb6851217
jedominguez <78039980+YELO-icy@users.noreply.github.com>
2021-11-19 14:15:03 -0700

    Update routes.js
    updated routes methods

--- routes/routes.js
+++ routes/routes.js
@@ -2,23 +2,36 @@ const {MongoClient, ObjectId} = require('mongodb');
 const url = "mongodb+srv://dbUser:Password@cluster0.0i6jz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(url)
 
-const databaseName = "MangerDB"
-const Profile = client.db(databaseName).collection("Profile");
-passedRegex = false;
+const databaseName = "MangerDB" //db name
+const Profile = client.db(databaseName).collection("Profile"); //create table called Profile
+passedRegex = false; //simple bool
+const bycrpt = require('bcrypt'); 
 
+<<<<<<< HEAD
+//this method is to be used to sign up user to db
+=======
+// this is  the homepage 
+exports.index = async(req, res) => {
+    res.render('index', {
+        title:'Home'
+    });
+};
+
+>>>>>>> 6e8cbeabbf8a5ccd1927e8b74f074820bb42b3e4
 exports.AddUser = async (req,res) =>{
-    await client.connect()
-
-    let ProfileDetails = {
-        UUID : ObjectId,
+    await client.connect() //start connection to db
+    pass = req.body.password; // password is equal to sign up pass field
+    const encryptedPass = SaltAndHash(pass); //salt and hash the user password
+    let ProfileDetails = { 
+        UUID : ObjectId, //UUID is unique Object Id
         name: req.body.name,
         email:req.body.email,
         
-        Security = {
-            password: Encrypta,
+        Security : {
+            password: encryptedPass,
             username:req.body.username,            
         },
-        Tasks = {
+        Tasks : {
             TaskID:ObjectId,
             Title:req.body.taskTitle,
             Description: req.body.Description,
@@ -33,13 +46,9 @@ exports.AddUser = async (req,res) =>{
             friend:ObjectId
         }
     }
-    const InsertUser = await Profile.insertOne(ProfileDetails);
-    client.close()
+    const InsertUser = await Profile.insertOne(ProfileDetails); //Inserting to db collection
+    client.close() //closing db
 };
-
-
-
-const bycrpt = require('bcrypt');
 
 exports.PullUser = async (req,res) => {
 
@@ -53,23 +62,25 @@ exports.DeleteTask = async (req,res) =>{
 const deleteT = (ObjectId) =>{
     await client.connect();
     const d = await Profile.deleteOne(Del => ProfileDetails.TaskID == ObjectId);
+    
     client.close();
     return d;
 };
 
-
+//Method is used in SaltAndHash method
 const checkRegex = (str) => {
+    //regex to verify
     let emailRegex = /^\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/;
     let passwordRegex = /(?=.[a-z])(?=.[0-9])(?=.[A-Z])(?=.[!@#$%^&])[a-zA-Z0-9!@#$%^&]{8,}/;
     let nameRegex = /.[a-z].[a-z].*/;
     let usernameRegex = /^[a-z0-9_-]{3,16}$/;
     let num = 0;
-
     passed = false;
+    //Switch case to check regex
     switch(num)
     {
         case 0:
-            if(nameRegex.test(str) == true)
+            if(nameRegex.test(str) == true) // test regex
             {
                 passed = true
                 num +=1;
@@ -102,11 +113,11 @@ const checkRegex = (str) => {
             
         break
     }
-    if(passed){ return passedRegex = true;}
+    if(passed){ return passedRegex = true; num= 0} //returns bool when passed
 };
-
+//Return salt and hash password
 const SaltAndHash = (Str) => {
-    if(checkRegex){
+    if(checkRegex(str)){
         const salt = bycrpt.genSaltSync(10);
         const hash = bycrpt.hashSync()
         return hash
@@ -116,8 +127,27 @@ const SaltAndHash = (Str) => {
     }
 };
 
+//To be used for Login page
+exports.Login = async(req,res) =>{
+    await client.connect();
+    UserUname = req.body.username
+    UserPassword = req.body.password
+    let usernameRegex = /^[a-z0-9_-]{3,16}$/;
 
-const checkAuth = (req) =>{
+    if(checkAuth && usernameRegex.test(UserUname))
+    {
+        const result = Profile.findOne({password:UserPassword}).toArray(1);
+        console.log(result);
+        return result
+    }
+};
+
+
+const checkAuth = (str) =>{
+    //Check if password matches
+    //start a session
+    //direct them to home page
+
     const auth = bycrpt.compare(req.body.password, SaltAndHash);
     if(auth)
     {
